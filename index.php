9ba558e18b6d8928e0d7b14452b01bfa98e1fb49
Alex <mildcloudy@gmail.com>
2013-01-16 21:54:27 -0500

    add index.php

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <body>
 	<div align="center">
 		<h2>Google</h2>
+		<h2>Hello World!</h2>
 		<form action="http://www.google.com/search" method="get">
 			<input type="text" name="q" size="50" maxlength="100"/><br/>
 			<input type="submit" value="Search" />
 			<input type="submit" value="I'm feeling lucky" />
 		</form>
 	</div>
+	<hr>
 </body>
 </html>
